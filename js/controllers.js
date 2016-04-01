290c845c1d1146bd6b19fb25443cb02379ecaa0d
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-04-01 11:05:40 -0500

    working!

--- js/controllers.js
+++ js/controllers.js
@@ -8,23 +8,21 @@ controllers.controller('PalindromePageController', ['$scope', '$rootScope', '$ht
 
     var mytext = $scope.myText;
     var reserText = '';
-    function isPalindrom(mytext) {
-        var i1 = 0;
-        var i2 = mytext.length - 1
-        while (i2 > i1) {
-            if (word[i1] != word[i2]) {
-                return false;
-            }
-            ++i1;
-            --i2;
-        }
-        return true;
+
+    function isPalindrome(myText) {
+        var removeChar = myText.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+
+        var checkPalindrome = removeChar.split('').reverse().join('');
+        
+        $scope.reserverText = checkPalindrome;
+
+        return (removeChar === checkPalindrome);
     }
 
-    if(isPalindrom(mytext)){
+    if (isPalindrome(mytext)) {
         alert('Is a Palindrome!');
-    }else{
-        alert('Not is a Palindrome!'); 
+    } else {
+        alert('Not is a Palindrome!');
     }
 
 }]);
